8e2fce680c68bc70993d23bdf72a168866f405f0
Louis Gitonga <louisgitonga19@gmail.com>
2026-04-16 17:53:41 +0300

    js

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("searchForm");
+const input = document.getElementById("searchInput");
+const result = document.getElementById("result");
+const errorMessage = document.getElementById("errorMessage");
+
+const wordEl = document.getElementById("word");
+const pronunciationEl = document.getElementById("pronunciation");
+const partOfSpeechEl = document.getElementById("partOfSpeech");
+const definitionEl = document.getElementById("definition");
+const exampleEl = document.getElementById("example");
+const synonymsEl = document.getElementById("synonyms");
+const audioEl = document.getElementById("audio");
