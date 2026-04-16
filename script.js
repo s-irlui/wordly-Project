b55aacf6c02868030505256e6aa4972cd60ab953
Louis Gitonga <louisgitonga19@gmail.com>
2026-04-16 18:05:05 +0300

    js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//elements
 const form = document.getElementById("searchForm");
 const input = document.getElementById("searchInput");
 const result = document.getElementById("result");
@@ -10,3 +11,32 @@ const definitionEl = document.getElementById("definition");
 const exampleEl = document.getElementById("example");
 const synonymsEl = document.getElementById("synonyms");
 const audioEl = document.getElementById("audio");
+
+//event listener
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const word = input.value.trim();
+    if (word === "") return;
+
+    fetchWord(word);
+});
+
+function fetchWord(word) {
+    errorMessage.classList.add("hidden");
+    result.classList.remove("show");
+
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Word not found");
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayWord(data[0]);
+        })
+        .catch(error => {
+            showError(error.message);
+        });
+}
